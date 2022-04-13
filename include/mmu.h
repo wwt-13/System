c449cdb538ed229b957a8c015049eace4ea8beda
wwt-13 <1270414897@qq.com>
2022-04-13 16:34:31 +0800

    又新增了很多东西，但是要完成全部的内容还需要很长时间，打算明天的lab2-1先不去了

--- include/mmu.h
+++ include/mmu.h
@@ -14,3 +14,74 @@ Part1. MIPS definitions
 */
 
 #define BY2PG 4096 // 定义了页大小
+
+/*
+Part2. Our conventions
+*/
+
+/*内存布局图,非常重要
+ o     4G ----------->  +----------------------------+------------0x100000000
+ o                      |       ...                  |  kseg3
+ o                      +----------------------------+------------0xe000 0000
+ o                      |       ...                  |  kseg2
+ o                      +----------------------------+------------0xc000 0000
+ o                      |   Interrupts & Exception   |  kseg1
+ o                      +----------------------------+------------0xa000 0000
+ o                      |      Invalid memory        |   /|\
+ o                      +----------------------------+----|-------Physics Memory Max
+ o                      |       ...                  |  kseg0
+ o  VPT,KSTACKTOP-----> +----------------------------+----|-------0x8040 0000-------end
+ o                      |       Kernel Stack         |    | KSTKSIZE            /|\
+ o                      +----------------------------+----|------                |
+ o                      |       Kernel Text          |    |                    PDMAP
+ o      KERNBASE -----> +----------------------------+----|-------0x8001 0000    |
+ o                      |   Interrupts & Exception   |   \|/                    \|/
+ o      ULIM     -----> +----------------------------+------------0x8000 0000-------
+ o                      |         User VPT           |     PDMAP                /|\
+ o      UVPT     -----> +----------------------------+------------0x7fc0 0000    |
+ o                      |         PAGES              |     PDMAP                 |
+ o      UPAGES   -----> +----------------------------+------------0x7f80 0000    |
+ o                      |         ENVS               |     PDMAP                 |
+ o  UTOP,UENVS   -----> +----------------------------+------------0x7f40 0000    |
+ o  UXSTACKTOP -/       |     user exception stack   |     BY2PG                 |
+ o                      +----------------------------+------------0x7f3f f000    |
+ o                      |       Invalid memory       |     BY2PG                 |
+ o      USTACKTOP ----> +----------------------------+------------0x7f3f e000    |
+ o                      |     normal user stack      |     BY2PG                 |
+ o                      +----------------------------+------------0x7f3f d000    |
+ a                      |                            |                           |
+ a                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                           |
+ a                      .                            .                           |
+ a                      .                            .                         kuseg
+ a                      .                            .                           |
+ a                      |~~~~~~~~~~~~~~~~~~~~~~~~~~~~|                           |
+ a                      |                            |                           |
+ o       UTEXT   -----> +----------------------------+                           |
+ o                      |                            |     2 * PDMAP            \|/
+ a     0 ------------>  +----------------------------+ -----------------------------
+ o
+*/
+/*
+Part3. Our helper functions
+*/
+#include "types.h"
+// 实现位于init.c
+void bcopy(const void *, void *, size_t);
+// 实现位于init.c
+void bzero(void *, size_t);
+
+// 暂时不理解
+extern char bootstacktop[], bootstack[];
+
+// 还有一个奇怪的问题没有解决:看到很多地方,头文件中都只有函数的定义而没有函数的实现,那为什么使用的时候只需要include"头文件"就能使用了呢(明明头文件里只有相关函数的定义呀)?
+/*下面是这个问题的具体解答
+
+*/
+// 定义物理页的大小
+extern u_long npage;
+
+typedef u_long Pde;
+typedef u_long Pte;
+
+#endif
+#endif
--- mm/pmap.c
+++ mm/pmap.c
@@ -14,7 +14,7 @@ u_long extmem;  // 扩展内存大小,本实验中不涉及,置0即可
 Pde *boot_pgdir; // Pde的重命名在mmu.h中,原类型是u_long
 
 struct Page *pages;
-static u_long freemem;
+static u_long freemem; // 貌似定义的是内存分配的初始地址
 
 static struct Page_list page_free_list;
 
@@ -42,9 +42,34 @@ void mips_detect_memory()
 static void *alloc(u_int n, u_int align, int clear)
 {
     // 下面对这些外部变量做详细解释
+    // end变量的定义位于tools/scse0_3.lds:end=0x80400000(总结后)
+    // end是虚拟地址,为kseg0,对应的物理地址是0x400000
+    // 所以我们管理的物理地址区间是[0x00400000,0x03ffffff]  (也就是说我们实际上只用了60MB的物理空间)
+    // 这里观察include/mmu.h中的内存布局图,可以发现很多东西
+    // 0x80000000-0x8001000存放的是中断异常相关的代码
+    // 0x80010000-0x8040000存放的就是内核代码(全连上了,64MB物理空间)
+    // 与之对应管理的虚拟内存区间是[0x80400000,0x83ffffff]
+    // 喵喵喵,那么这样的话不是说我们管理的内存空间完全不需要考虑TLB映射的问题了吗???
     extern char end[];
-    extern int mCONTEXT;
-    extern struct Env *envs;
+    u_long alloced_mem; // 初始地址
+
+    // 如果是第一次分配内存空间的话,需要将freemem置为地址的起始点
+    if (freemem == 0)
+    {
+        freemem = (u_long)end;
+    }
+    // 使得freemem向上取整能够被align整除(一般用于页对齐,页大小0x1000)
+    freemem = ROUND(freemem, align);
+    // 对齐后的freemem才是应该返回的初始地址
+    alloced_mem = freemem;
+    // 分配n个字节的空间(跨页分配没关系！！)
+    freemem = freemem + n;
+    // 如果分配的地址超出了最大物理地址,则抛出异常
+    if (PADDR(freemem) >= maxpa)
+    {
+        panic("out of memory\n");
+        return (void *)-E_NO_MEM;
+    }
 }
 static Pte *boot_pgdir_walk(Pde *pgdir, u_long va, int create)
 {
@@ -55,4 +80,7 @@ void boot_map_segment(Pde *pgdir, u_long va, u_long size, u_long pa, int perm)
 
 void mips_vm_init()
 {
+    extern char end[];
+    extern int mCONTEXT;
+    extern struct Env *envs;
 }
